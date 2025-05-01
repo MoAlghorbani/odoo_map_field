f45705e5d31993d0ca25ba43a2ab803843aa43a6
Mohammed Fadl <mohammadefathl@gmail.com>
2025-05-01 13:22:12 +0300

    init app

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Map Field",
+    "version": "17.0.0.2",
+    "summary": "Map Field",
+    "sequence": 10,
+    "depends": ["base", "web"],
+    "installable": True,
+    "application": True,
+    "price": 10.00,
+    "data": ["views/map_setting_view.xml", "security/ir.model.access.csv"],
+    "assets": {
+        "web.assets_backend": [
+            "map_field/static/src/**/*",
+        ],
+    },
+    "license": "OPL-1",
+}
